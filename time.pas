8b0556dd933571949d4bf5cb3cc41473ce82d5fc
Chetvertakov Maxim <chetvertakovmaxim@gmail.com>
2023-09-30 09:31:20 +0300

    days

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var year:=readinteger('введи год:');
     if (year mod 4 = 0) and not((year mod 100 = 0) and not(year mod 400 = 0)) then
-      print('Високосный')
+      print('Високосный','days=',366)
     else
-      print('невисокосный');
+      print('невисокосный','days=',365);
 end.
