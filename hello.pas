4c9e17a19f2bc505fd7b51a9359fbea70a34234b
Chetvertakov Maxim <chetvertakovmaxim@gmail.com>
2023-09-30 09:06:44 +0300

    dobriy den

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  print('hello world!');
+  var a:=readinteger('Который час?');
+  case a of
+    4..10:print('Доброе утро,мир!');
+    11..16:print('Добрый день,мир!');
+    17..22:print('Добрый вечер,мир!')
+    else
+      print('Доброй ночи,мир!');
+  end;
 end.
+//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
